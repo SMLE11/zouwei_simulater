f64732be122742bd6e594767bb7e3e2a499d4ed8
胤汝 陈 <1048399390@qq.com>
2022-09-30 08:16:06 +0800

    9.30 8:15

--- ship.py
+++ ship.py
@@ -3,6 +3,7 @@
 from math import sqrt
 
 import pygame as pg
+from pygame import freetype
 
 
 class Ship:
@@ -12,48 +13,25 @@ class Ship:
         self.image = pg.transform.scale(pg.image.load('images/ship.JPG'), (20, 20))
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
-        self.move_right = False
-        self.move_up = False
-        self.move_down = False
-        self.move_left = False
         self.speed = ai_game.setting.ship_speed
-        self.move_to_pos = pg.Vector2(self.rect.x, self.rect.y)
-        self.now_pos = self.move_to_pos
+        self.move_to_pos = pg.Vector2(1070,780)
+        self.now_pos = pg.Vector2(self.rect.x, self.rect.y)
         self.setting = ai_game.setting
 
     def blitme(self):
-        self.screen.blit(self.image, self.rect)
+        self.screen.blit(self.image, self.now_pos)
 
     def update(self):
         self.move()
 
     def move(self):
-        dist = self.move_to_pos-self.now_pos
-        dist_len=dist.length()
-        if dist_len:
+        dist = self.move_to_pos - self.now_pos
+        dist_len = dist.length()
+        if dist_len>self.setting.ship_speed:
             dist.normalize_ip()
-            step = self.setting.ship_speed*dist
-            v_x = abs(step.x)
-            v_y = abs(step.y)
+            step = self.setting.ship_speed * dist
+            self.now_pos+=step
         else:
-            v_x=0
-            v_y=0
-        if self.rect.x > self.move_to_pos.x:
-            self.rect.x -= v_x
-            if self.rect.x < self.move_to_pos.x:
-                self.rect.x = self.move_to_pos.x  # 修正位置
-        else:
-            self.rect.x += v_x
-            if self.rect.x > self.move_to_pos.x:
-                self.rect.x = self.move_to_pos.x  # 修正位置
-
-        if self.rect.y > self.move_to_pos.y:
-            self.rect.y -= v_y
-            if self.rect.y < self.move_to_pos.y:
-                self.rect.y = self.move_to_pos.y  # 修正位置
-        else:
-            self.rect.y += v_y
-            if self.rect.y > self.move_to_pos.y:
-                self.rect.y = self.move_to_pos.y  # 修正位置
+            self.now_pos=self.move_to_pos
 
 
